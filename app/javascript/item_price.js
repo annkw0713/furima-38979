51e4814c48cde649f244777af16bc9d4973ed594
annkw0713 <annkw75239549@gmail.com>
2023-03-16 12:12:05 +0900

    カンマでの区切り表示追加実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -4,7 +4,7 @@ window.addEventListener('load', () => {
     const inputValue = priceInput.value;
     const addTaxPriceOutput = document.getElementById("add-tax-price");
     const profitOutput = document.getElementById("profit");
-    addTaxPriceOutput.innerHTML = Math.floor(inputValue * 0.1);
-    profitOutput.innerHTML = inputValue - (Math.floor(inputValue * 0.1));
+    addTaxPriceOutput.innerHTML = Math.floor(inputValue * 0.1).toLocaleString();
+    profitOutput.innerHTML = (inputValue - (Math.floor(inputValue * 0.1))).toLocaleString();
   });
 });
