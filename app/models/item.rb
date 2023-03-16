32f596b73efc2811ebd77a57da705326ff31a0d9
annkw0713 <annkw75239549@gmail.com>
2023-03-16 12:04:36 +0900

    価格のバリデーション追加
    半角数値のみ保存可能

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,7 +16,7 @@ class Item < ApplicationRecord
   validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank" }
   validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank" }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ }
   validates :image, presence: true
 
 end
