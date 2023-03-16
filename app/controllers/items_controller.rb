80b48230a2bb2ab4db1ad2cf0ca47bcec87d4078
annkw0713 <annkw75239549@gmail.com>
2023-03-16 15:39:22 +0900

    コントローラー記述内容の修正
    ・商品一覧機能に関する記述のコメントアウト
    ・createアクション内の記述修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   # before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.all
+    # @items = Item.all
   end
 
   def new
@@ -11,7 +11,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
